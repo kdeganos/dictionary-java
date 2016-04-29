3962067ba428ffd6daec23f6bd1d0609ebde5733
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-04-29 08:20:29 -0700

    Initial project setup

---
+++ src/main/java/Word.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Word{
+
+  public Boolean runBlank (Integer year) {
+     // eventually put your code here
+     return false;
+  }
+}
---
+++ src/test/java/WordTest.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class WordTest {
+
+ @Test
+ public void runBlank_CheckifPageIsBlank_true() {
+   Blank myBlank = new Blank();
+   assertEquals(FirstArgument, SecondArgument());
+ }
+}
